502f90f28a224cead02d46a780768abdff63e2aa
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-03-30 22:02:00 +0300

    Styled the homePage

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
-  $("#move").click(function(){
-      // $("html,body").animate({scrollTop:$("#form").scrollTop()},1000);
-      // return false;
-       $("html,body").animate({scrollTop:$(document).height()},2000);
-       return false;
-  })
+  
 });
