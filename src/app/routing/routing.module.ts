4d3e2260145446bd27606b1847310db2ae8ecef9
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-03-31 11:38:59 +0300

    This is to add routing links for easy navigation

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule ,Routes } from "@angular/router";
 
+import { GithubComponent } from "../github/github.component";
+import { UserWelcomeComponent } from "../user-welcome/user-welcome.component";
+
+const routes:Routes=[
+  {path:"github",component:GithubComponent},
+  {path:"home",component:UserWelcomeComponent},
+  {path:"",redirectTo:"/home",pathMatch:"full"}
+]
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class RoutingModule { }
