9add36d267f57a53dd02d0b2538480d6f7a01e8a
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-03-31 22:13:52 +0300

    This is to change the position of div loader

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value:any): number {
+    let today:Date=new Date();
+    let todayWithNotime:any=new Date(todat.getFullYear(),today.getMonth(),today.getDate());
+    var dateDifference=Math.abs(value-todayWithNotime);
+    const daysInSeconds=86400;
+    var dateDifferentSeconds=dateDifferencw*0.001;
+    var dateCounter=dateDifferentSeconds/daysInSeconds;
+    if(dateCounter>=1 && value >todayWithNotime){
+      return dateCounter;
+    }
+    else{
+      return 0;
+    }
+
   }
 
 }
