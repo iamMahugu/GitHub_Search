285e19a326ceb553ee58a6e7894f385f6d2f6e79
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-03-31 17:04:35 +0300

    This is to add a loop function in typed js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
     $("html,body").animate({scrollTop:$("#card").height()},2000);
       return false;
   })
+  var typed=new Typed(`#type`,{
+      strings:["Search Repos","Search Users","Search user Names","All in one","Hit the button below to get started"],
+      backSpeed:70,
+      typeSpeed:80,
+      loop:1
+    })
 });
